1b94023c57b468fb0449d59a080e121ad4c33c54
astasovsky <stasovsky.ua@gmail.com>
2023-05-14 17:46:26 +0300

    Laboratory work 5 tasks. Done

---
+++ Assets/Scripts/BallMovement.cs
@@ -0,0 +1,76 @@
+﻿using DG.Tweening;
+using GG.Infrastructure.Utils.Swipe;
+using UnityEngine;
+
+public class BallMovement : MonoBehaviour
+{
+    [SerializeField] private SwipeListener swipeListener;
+    [SerializeField] private LevelManager levelManager;
+
+    [SerializeField] private float stepDuration = 0.1f;
+    [SerializeField] private LayerMask wallsAndRoadsLayer;
+    private const float MaxRayDistance = 10f;
+
+    private Vector3 _moveDirection;
+    private bool _canMove = true;
+
+    private void Start()
+    {
+        transform.position = levelManager.defaultBallRoadTile.position;
+
+        swipeListener.OnSwipe.AddListener(swipe =>
+        {
+            _moveDirection = swipe switch
+            {
+                "Right" => Vector3.right,
+                "Left" => Vector3.left,
+                "Up" => Vector3.forward,
+                "Down" => Vector3.back,
+                _ => _moveDirection
+            };
+            Debug.Log(swipe);
+
+            MoveBall();
+        });
+    }
+
+    private void MoveBall()
+    {
+        if (_canMove)
+        {
+            _canMove = false;
+
+            RaycastHit[] hits = Physics.RaycastAll(transform.position, _moveDirection, MaxRayDistance,
+                wallsAndRoadsLayer.value);
+
+            Vector3 targetPosition = transform.position;
+
+            int steps = 0;
+            for (int i = 0; i < hits.Length; i++)
+            {
+                if (hits[i].collider.isTrigger)
+                {
+                }
+                else
+                {
+                    // Wall tile
+                    if (i == 0)
+                    {
+                        // means wall is near the ball
+                        _canMove = true;
+                        return;
+                    }
+
+                    //else:
+                    steps = i;
+                    targetPosition = hits[i - 1].transform.position;
+                    break;
+                }
+            }
+
+            float moveDuration = stepDuration * steps;
+
+            transform.DOMove(targetPosition, moveDuration).SetEase(Ease.OutExpo).OnComplete(() => _canMove = true);
+        }
+    }
+}
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelManager : MonoBehaviour
@@ -5,34 +6,44 @@ public class LevelManager : MonoBehaviour
     [Header("Level texture")] [SerializeField]
     private Texture2D levelTexture;
 
-    [Header("Titles Prefabs")] [SerializeField]
+    [Header("Tiles Prefabs")] [SerializeField]
     private GameObject prefabWallTile;
 
     [SerializeField] private GameObject prefabRoadTile;
 
+    [HideInInspector] public List<RoadTile> roadTilesList = new();
+    [HideInInspector] public RoadTile defaultBallRoadTile;
+
     private readonly Color _colorWall = Color.white;
     private readonly Color _colorRoad = Color.black;
+
     private float _unitPerPixel;
 
     private void Awake()
     {
         Generate();
+        defaultBallRoadTile = roadTilesList[0];
     }
 
     private void Generate()
     {
         _unitPerPixel = prefabWallTile.transform.lossyScale.x;
-        float halfUnitePerPixel = _unitPerPixel / 2;
-        float wight = levelTexture.width;
+        float halfUnitPerPixel = _unitPerPixel / 2f;
+
+        float width = levelTexture.width;
         float height = levelTexture.height;
-        Vector3 offset = (new Vector3(wight / 2f, 0f, height / 2f) * _unitPerPixel) - new
-            Vector3(halfUnitePerPixel, 0f, halfUnitePerPixel);
-        for (int x = 0; x < wight; x++)
+
+        Vector3 offset = new Vector3(width / 2f, 0f, height / 2f) * _unitPerPixel -
+                         new Vector3(halfUnitPerPixel, 0f, halfUnitPerPixel);
+        for (int x = 0; x < width; x++)
         {
             for (int y = 0; y < height; y++)
             {
+                //Get pixel color :
                 Color pixelColor = levelTexture.GetPixel(x, y);
-                Vector3 spawnPos = (new Vector3(x, 0f, y) * _unitPerPixel) - offset;
+
+                Vector3 spawnPos = new Vector3(x, 0f, y) * _unitPerPixel - offset;
+
                 if (pixelColor == _colorWall)
                     Spawn(prefabWallTile, spawnPos);
                 else if (pixelColor == _colorRoad)
@@ -43,7 +54,12 @@ public class LevelManager : MonoBehaviour
 
     private void Spawn(GameObject prefabTile, Vector3 position)
     {
+        //fix Y position:
         position.y = prefabTile.transform.position.y;
+
         GameObject obj = Instantiate(prefabTile, position, Quaternion.identity, transform);
+
+        if (prefabTile == prefabRoadTile)
+            roadTilesList.Add(obj.GetComponent<RoadTile>());
     }
 }
